8ce940ce4fa82660f230783950e07575036c9399
jkermond-gmail <jkermond@gmail.com>
2018-11-05 16:22:50 -0500

    Complete working Chapter 1

--- AspNetCoreVideo/Startup.cs
+++ AspNetCoreVideo/Startup.cs
@@ -6,19 +6,37 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using AspNetCoreVideo.Services;
 
 namespace AspNetCoreVideo
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // ex 1 services.AddSingleton<IMessageService, HardCodedMessageService>();
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IMessageService, ConfigurationMessageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageService msg)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +45,10 @@ namespace AspNetCoreVideo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                // ex 1 await context.Response.WriteAsync("Hello, from my World, yeah!");
+                // ex 2 var message = Configuration["Message"];
+                // ex 2 await context.Response.WriteAsync(message);
+                await context.Response.WriteAsync(msg.GetMessage());
             });
         }
     }
