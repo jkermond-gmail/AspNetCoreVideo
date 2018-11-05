8919f554486ec5d488b7ff39860bfc7375260e60
jkermond-gmail <jkermond@gmail.com>
2018-11-05 16:25:20 -0500

    cleanup

--- AspNetCoreVideo/Startup.cs
+++ AspNetCoreVideo/Startup.cs
@@ -25,7 +25,6 @@ namespace AspNetCoreVideo
             Configuration = builder.Build();
         }
 
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
